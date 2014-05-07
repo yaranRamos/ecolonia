cdd77964b03f3f4cd30904da0f0657c522f103d8
yaranRamos <yaran.ramos.65@gmail.com>
2014-05-07 18:28:52 -0500

    version correcta

--- application/controllers/administrador.php
+++ application/controllers/administrador.php
@@ -6,15 +6,12 @@ class Administrador extends CI_Controller {
 		parent::__construct();
 		$this->load->model('casa_model');
 		$this->load->model('colono_model');
-<<<<<<< HEAD
 		$this->load->model('colonia_model');
 		$this->load->model('comite_model');
 		$this->load->model('calle_model');
 		$this->load->model('comitecolono_model');
-=======
 		$this->load->model('estado_model');
 		$this->load->model('municipio_model');
->>>>>>> b877000b157fe3e479bdc24d6fc55c18674904be
 	}
 
 	public function index()
@@ -131,63 +128,62 @@ class Administrador extends CI_Controller {
 		}
 	}
 
-<<<<<<< HEAD
 	public function registrar_comites(){
-			if ($this->input->post()) {
+		if ($this->input->post()) {
+			$estado=$this->input->post('estado');
+			$municipio=$this->input->post('municipio');
+			$colonia=$this->input->post('colonia');
+			//insertar datos del comite
+			$comites=$this->comite_model->registra_comite($colonia,
+												 $this->input->post('fundacion'),
+												 $this->input->post('comite'),
+												 $this->input->post('integrantes'));
+			if($comites){
+				$resultado = $this->comite_model->obtiene_id($this->input->post('comite'),$this->input->post('integrantes'));
+				$id_comite = $resultado->Id;
+				//insertar datos de la casa
+				$familia=$this->input->post('apaterno');
+				$calle=$this->input->post('calle');
+				//insertar calle de la colonia
+			    $this->calle_model->registra_calle($calle,$colonia);
+			    $id_calle = $this->calle_model->obtiene_id($calle,$colonia);
+			    $id_calleB = $id_calle->Id;
+				$numero=$this->input->post('numero');
+				$tel_casa = "";
+				$this->casa_model->registra_casa($id_calleB,$colonia,$familia,$numero,$tel_casa);
+				$casa = $this->casa_model->obtener_id($id_calleB,$colonia,$familia,$numero,$tel_casa);
+				$Casa = $casa->id;
 				
-				$estado=$this->input->post('estado');
-				$municipio=$this->input->post('municipio');
-				$colonia=$this->input->post('colonia');
-				//insertar datos del comite
-				$comites=$this->comite_model->registra_comite($colonia,
-													 $this->input->post('fundacion'),
-													 $this->input->post('comite'),
-													 $this->input->post('integrantes'));
-				if($comites){
-					$resultado = $this->comite_model->obtiene_id($this->input->post('comite'),$this->input->post('integrantes'));
-					$id_comite = $resultado->Id;
-					//insertar datos de la casa
-					$familia=$this->input->post('apaterno');
-					$calle=$this->input->post('calle');
-					//insertar calle de la colonia
-				    $this->calle_model->registra_calle($calle,$colonia);
-				    $id_calle = $this->calle_model->obtiene_id($calle,$colonia);
-				    $id_calleB = $id_calle->Id;
-					$numero=$this->input->post('numero');
-					$tel_casa = "";
-					$this->casa_model->registra_casa($id_calleB,$colonia,$familia,$numero,$tel_casa);
-					$casa = $this->casa_model->obtener_id($id_calleB,$colonia,$familia,$numero,$tel_casa);
-					$Casa = $casa->id;
-					
-					//insertar datos del colono
-					$ApellidoPaterno = $this->input->post('apaterno');
-					$ApellidoMaterno = $this->input->post('amaterno');
-					$FechaNacimiento = "";
-					$Estatura = "";
-					$Nombre = $this->input->post('nombre');
-					$Peso = "";
-					$Email = $this->input->post('correo');
-					$Sexo = $this->input->post('sexo');
-					$Tel_celular = $this->input->post('cel');
-					$this->colono_model->inserta_colono($Casa,$ApellidoPaterno,$ApellidoMaterno,$FechaNacimiento,$Estatura,$Nombre,$Peso,$Email,$Sexo,$Tel_celular);
-					$colono = $this->colono_model->obtiene_id($Email,$Tel_celular);
-					$id_colono = $colono->Id;
-					//insertar presidente de comite
-					$Puesto=1;
-					$this->comitecolono_model->registrapresidente($id_comite,$id_colono,$Puesto);
-				}else{
+				//insertar datos del colono
+				$ApellidoPaterno = $this->input->post('apaterno');
+				$ApellidoMaterno = $this->input->post('amaterno');
+				$FechaNacimiento = "";
+				$Estatura = "";
+				$Nombre = $this->input->post('nombre');
+				$Peso = "";
+				$Email = $this->input->post('correo');
+				$Sexo = $this->input->post('sexo');
+				$Tel_celular = $this->input->post('cel');
+				$this->colono_model->inserta_colono($Casa,$ApellidoPaterno,$ApellidoMaterno,$FechaNacimiento,$Estatura,$Nombre,$Peso,$Email,$Sexo,$Tel_celular);
+				$colono = $this->colono_model->obtiene_id($Email,$Tel_celular);
+				$id_colono = $colono->Id;
+				//insertar presidente de comite
+				$Puesto=1;
+				$this->comitecolono_model->registrapresidente($id_comite,$id_colono,$Puesto);
+			} else{
 				echo json_encode($comites);
-				}
-			}else{
+			}
+		} else{
 			$resp = false;
 			echo json_encode($resp);
-=======
+		}
+	}
+	
 	public function get_municipios(){
 		if($this->input->post()){
 			$id_estado = $this->input->post('estado_id');
 			$municipios = $this->municipio_model->get_municipios($id_estado);
 			echo json_encode($municipios);
->>>>>>> b877000b157fe3e479bdc24d6fc55c18674904be
 		}
 	}
 }
